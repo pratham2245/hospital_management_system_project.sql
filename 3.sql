1695400e460efeacb4d73305c161e672e30729e0
pratham2245 <prathamshinde2204@gmail.com>
2025-09-02 01:36:40 +0530

    Delete 3.sql

--- 3.sql
+++
@@ -1,7 +0,0 @@
--- List the names and contact numbers of all patients registered in 2022.
-SELECT 
-    first_name, last_name, contact_number
-FROM
-    patients
-WHERE
-    registration_date BETWEEN '2022-01-01' AND '2022-12-31';
