720296caa7b382b12ea1a406c7f282972dfffbab
pratham2245 <prathamshinde2204@gmail.com>
2025-09-02 01:37:40 +0530

    Delete 8.sql

--- 8.sql
+++
@@ -1,7 +0,0 @@
--- What is the monthly trend of new patient registrations?
-SELECT 
-    DATE_FORMAT(registration_date, '%Y-%m') AS month_year,
-    COUNT(*) AS patient_count
-FROM patients
-GROUP BY DATE_FORMAT(registration_date, '%Y-%m')
-ORDER BY month_year;
